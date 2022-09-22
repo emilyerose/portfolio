50a373c9e22f7e7145c47f9d0163576a752aae1f
emilyerose <emilyrose0021@gmail.com>
2022-09-21 21:20:31 -0400

    basic structure

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import logo from './logo.svg';
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+
+const App = () => <PortfolioContainer />;
+
+export default App;
---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        I am an innovative young software engineer with a background in astrophysics and full-stack coding. I got my bachelor's degree from Pomona College  and worked at the Jet Propulsion Laboratory for one year before deciding to switch to web coding.
+        I did a 3-month intensive course through the University of Pennsylvania to brush off my skills, and I'm now seeking employment as a full-stack developer. 
+        In my free time, I enjoy hiking, mountain biking, skiing, and going to live performance. Talk to me about dance, musicals, or live music if you want an earful!
+      </p>
+    </div>
+  );
+}
